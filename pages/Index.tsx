9723b0456c46e94391b740662a9953308e3356b8
Niosyle <nhelt.infra@gmail.com>
2026-01-12 21:23:32 +0100

    Update Index.tsx

--- pages/Index.tsx
+++ pages/Index.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
+import Recommendations from "@/components/Recommendations";
 import Contact from "@/components/Contact";
 
 const Index = () => {
@@ -14,6 +15,7 @@ const Index = () => {
         <Experience />
         <Skills />
         <Projects />
+        <Recommendations />
         <Contact />
       </main>
       
